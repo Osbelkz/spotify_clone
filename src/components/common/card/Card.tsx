62388bf10aff9b6b4afdd84d8f054839b784f3e0
Askar <osbelkz@gmail.com>
2021-01-24 23:03:46 +0600

    feat: show image after full load card component

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import classes from "./Card.module.scss";
 
@@ -6,18 +6,24 @@ type PropsType = {
     imageSrc: string,
     link: string,
     name: string,
-    description: string |  JSX.Element | null
+    description: string | JSX.Element | null
 }
 
 const Card: React.FC<PropsType> = ({link, name, description, imageSrc}) => {
 
+    let [imageLoadComplete, setImageLoadComplete] = useState(false)
+
+
     return (
         <div className={classes.card}>
-            <div>
-                <Link to={{pathname: link}}>
-                    <img className={classes.hover} src={imageSrc} alt=""/>
-                </Link>
-            </div>
+            <Link to={{pathname: link}}>
+                <div className={classes.imageWrapper}>
+                    <img className={classes.hover}
+                         style={{opacity: imageLoadComplete ? "1" : "0"}}
+                         onLoad={() => setImageLoadComplete(true)}
+                         src={imageSrc} alt=""/>
+                </div>
+            </Link>
 
             <p className={classes.name}>{name}</p>
             <p className={classes.description}>
